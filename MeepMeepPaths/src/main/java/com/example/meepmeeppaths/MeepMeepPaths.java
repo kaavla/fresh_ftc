869f680f8a689f8ac3d5fe6fee36467ecc1e8d23
Shashank Khanvilkar <kaavla@gmail.com>
2022-02-19 10:45:41 -0800

    Added meepmeep

---
+++ MeepMeepPaths/src/main/java/com/example/meepmeeppaths/MeepMeepPaths.java
@@ -0,0 +1,52 @@
+package com.example.meepmeeppaths;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequence;
+import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequenceBuilder;
+
+import java.util.ArrayList;
+
+public class MeepMeepPaths {
+
+	static double robotLength = 18;
+	static double robotWidth = 11.5;
+
+	static final double tileSize = 23;
+	static final double tileConnector = 0.75;
+
+	static final double hubRadius = 9;
+
+	static final double cameraRightIndent = 1.25;
+
+	public static void main( String[] args ) {
+		//BlueInAutoDuck rt = new BlueInAutoDuck();
+		//BlueInAutoFreight2 rt = new BlueInAutoFreight2();
+		//BlueOutAutoDuck rt = new BlueOutAutoDuck();
+		//BOutDuckTest rt = new BOutDuckTest();
+		//PositionGrapherTest rt = new PositionGrapherTest();
+		//TechnovaKillerAuto rt = new TechnovaKillerAuto();
+		ShankRed1 rt = new ShankRed1();
+		//ShankBlue1 rt = new ShankBlue1();
+		MeepMeep mm = new MeepMeep( 700 )
+				.setBackground( MeepMeep.Background.FIELD_FREIGHT_FRENZY )
+				.setTheme( new ColorSchemeRedDark() )
+//				.set
+				.setBackgroundAlpha( 1f )
+				.setBotDimensions( robotWidth, robotLength )
+				.setConstraints( 30, 30, Math.toRadians( 60 ), Math.toRadians( 60 ), 17 )
+				.followTrajectorySequence( drive -> {
+					//try {
+						return rt.getTrajectorySequence( drive );
+					//} catch( InstantiationException | IllegalAccessException | ClassNotFoundException e ) {
+					//	e.printStackTrace( );
+				//	}
+					//return new TrajectorySequence( new ArrayList<>(  ) );
+				} )
+				.start( );
+	}
+
+
+}
